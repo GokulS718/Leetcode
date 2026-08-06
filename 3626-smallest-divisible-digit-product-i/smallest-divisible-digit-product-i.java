baab82f270f02888322bc705c01fcd73e7ff913a
GokulS718 <gokulkavya149@gmail.com>
2026-08-06 09:27:33 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (50.26%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        for(int i=n;i<=100;i++){
+            int prod =1;
+            int j=i;
+            while(j!=0){
+                int rem = j%10;
+                prod = prod*rem;
+                j/=10;
+            }
+            if(prod%t ==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
