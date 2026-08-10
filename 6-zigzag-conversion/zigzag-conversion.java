bcb6094ebe1a71c449d07b5631f2ffb3830575fe
GokulS718 <gokulkavya149@gmail.com>
2026-08-10 09:40:28 +0530

    Time: 5 ms (52.50%) | Memory: 46.6 MB (62.32%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,29 +1,25 @@
 class Solution {
     public String convert(String s, int numRows) {
-        StringBuilder[] sb = new StringBuilder[numRows];
-        for(int i=0;i< numRows;i++){
-            sb[i]=new StringBuilder();
-        }
-
-        if(numRows==1 || s.length()<=numRows){
+        if(numRows==1 || numRows>=s.length()){
             return s;
         }
-
-        int rows=0;
-        boolean found=false;
-        for(char ch : s.toCharArray()){
-           sb[rows].append(ch);
-           if(rows==0 || rows == numRows-1){
-              found = !found;
-           }
-        //    rows += found?1:-1;
-        if(found) rows++;
-        else rows--;
+        StringBuilder[] sb = new StringBuilder[numRows ];
+        for(int i=0;i<numRows;i++){
+            sb[i]=new StringBuilder();
         }
-        StringBuilder res = new StringBuilder();
-        for(int i=0; i < numRows;i++){
-            res.append(sb[i]);
+        boolean updown = false;
+        int row=0;
+        for(int i=0;i<s.length();i++){
+            sb[row].append(s.charAt(i));
+            if(row==0 || row==numRows-1){
+                updown = !updown;
+            }
+            row+= updown?1:-1;
         }
-        return res.toString();
+         StringBuilder gd = new StringBuilder();
+        for(int i=0;i<sb.length;i++){
+           gd.append(sb[i]);
+        }
+        return new String(gd);
     }
 }
