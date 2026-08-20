9575af18be43ca5fd8d19b9efe4a4ecb9d791f31
GokulS718 <gokulkavya149@gmail.com>
2026-08-20 11:15:05 +0530

    Time: 2 ms (38.19%) | Memory: 42.8 MB (75.22%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        int fact =1;
+        for(int i=1;i<n;i++){
+            list.add(i);
+            fact*=i;
+        } 
+        list.add(n);
+        k--;
+        StringBuilder sb = new StringBuilder();
+        while(n>0){
+             int index = k/fact;
+             sb.append(list.get(index));
+             list.remove(index);
+
+             k = k % fact;
+             n--;
+
+            if(n>0) fact = fact/n;
+        }
+        return sb+"";
+    }
+}
