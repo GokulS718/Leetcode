d87b55c78fc51cba823e144abdb94761cc609f2c
GokulS718 <gokulkavya149@gmail.com>
2026-09-01 10:47:37 +0530

    Time: 1 ms (98.88%) | Memory: 47 MB (8.11%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        while(k>0){
+            
+        int index=0;
+            for(int i=1;i<nums.length;i++){
+                if(nums[i]<nums[index]){
+                    index =i;
+                }
+            }
+            nums[index]= nums[index]*multiplier;
+            k--;
+        }
+        return nums;
+    }
+}
