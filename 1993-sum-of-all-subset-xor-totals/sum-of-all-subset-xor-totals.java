6bb9e1785952a1f5754f8f9398cd623674ab8f14
GokulS718 <gokulkavya149@gmail.com>
2026-09-02 09:51:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (76.67%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,9 +1,13 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int total =0;
-        for(int num : nums){
-            total |= num;
-        }
-        return total << (nums.length-1);
+        return solve(nums , 0 ,0);
+    }
+    public int solve(int[] nums , int index , int total){
+        if(index == nums.length) return total;
+
+        int in = solve(nums , index+1 , nums[index] ^ total);
+        int out = solve(nums , index+1 , total);
+
+        return in+out;
     }
 }
